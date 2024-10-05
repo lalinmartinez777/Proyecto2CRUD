06f969c2f7941d59dea8a41e0e36536ca25e5676
Manuel Eduardo Martinez Ramirez <lalofrancia1@gmail.com>
2024-10-05 17:51:45 -0600

    crear tabla de libros

---
+++ database/migrations/2024_10_05_233936_create_libros_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('libros', function (Blueprint $table) {
+            $table->id(); // Clave primaria autoincremental
+            $table->string('nombre_libro'); // Nombre del libro
+            $table->string('nombre_autor'); // Nombre del autor
+            $table->year('ano'); // Año de publicación
+            $table->integer('copias_disponibles'); // Número de copias disponibles
+            $table->integer('paginas'); // Número de páginas del libro
+            $table->boolean('disponible_envio'); // Disponible para envío (0 o 1)
+            $table->timestamps(); // Columnas created_at y updated_at
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('libros');
+    }
+};
